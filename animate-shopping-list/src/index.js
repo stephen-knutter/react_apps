d3d3ce39d348a199e2e654b8294cdbcb3cd3893c
smkqp8 <stephen.knutter@gmail.com>
2016-10-19 21:25:57 -0600

    animate shopping list

---
+++ animate-shopping-list/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
+import './index.css';
+
+ReactDOM.render(
+  <ShoppingList />,
+  document.getElementById('root')
+);
