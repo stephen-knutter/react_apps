d4dd80ebeac1b5494f9e52156afa42f522414d16
smkqp8 <stephen.knutter@gmail.com>
2016-11-12 09:59:47 -0700

    add dnd

---
+++ drag-n-drop/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Container from './Containers/App/Container';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <Container />
+    )
+  }
+}
+
+export default App;
---
+++ drag-n-drop/src/Components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,52 @@
+import React, {PropTypes, Component} from 'react';
+import {DropTarget} from 'react-dnd';
+import constants from '../../constants';
+
+const ShoppingCartSpec = {
+  drop() {
+    return {name: 'ShoppingCart'};
+  }
+};
+
+let collect = (connect, monitor) => {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+}
+
+class ShoppingCart extends Component {
+  render() {
+    const {canDrop, isOver, connectDropTarget} = this.props;
+    const isActive = canDrop && isOver;
+
+    let backgroundColor = '#ffffff';
+
+    if (isActive) {
+      backgroundColor = '#f7f7bd';
+    } else if (canDrop) {
+      backgroundColor = '#f7f7f7';
+    }
+
+    const style = {
+      backgroundColor: backgroundColor
+    };
+
+    return connectDropTarget(
+      <div className='shopping-cart' style={style}>
+        {
+          isActive ? 'Hummm, snack!' : 'Drag here!'
+        }
+      </div>
+    );
+  }
+}
+
+ShoppingCart.propTypes = {
+  connectDropTarget: PropTypes.func.isRequired,
+  isOver: PropTypes.bool.isRequired,
+  canDrop: PropTypes.bool.isRequired
+}
+
+export default DropTarget(constants.SNACK, ShoppingCartSpec, collect)(ShoppingCart);
