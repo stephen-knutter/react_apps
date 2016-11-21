af7bc7eecccb22ef87b13e7f9d68efd18cb304ea
smkqp8 <stephen.knutter@gmail.com>
2016-11-21 16:28:09 -0700

    finish redux bank

---
+++ redux-bank/app/App.js
@@ -0,0 +1,62 @@
+import React, {Component, PropTypes} from 'react';
+import {render} from 'react-dom';
+import bankStore from './bankStore';
+import constants from './constants';
+import bankActionCreators from './bankActionCreators';
+
+class BankApp extends Component {
+  handleDeposit() {
+    this.props.onDeposit(this.refs.amount.value);
+    this.refs.amount.value = '';
+  }
+
+  handleWithdraw() {
+    this.props.onWithdraw(this.refs.amount.value);
+    this.refs.amount.value = '';
+  }
+
+  render() {
+    return(
+      <div>
+        <header>
+          <img src="logo.svg" width="150" />Redux Bank
+        </header>
+        <h1>Your balance is ${(this.props.balance).toFixed(2)}</h1>
+        <div className="atm">
+          <input type="text" placeholder="Enter Amount" ref="amount" />
+          <button onClick={this.handleWithdraw.bind(this)}>Withdraw</button>
+          <button onClick={this.handleDeposit.bind(this)}>Deposit</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+BankApp.propTypes = {
+  balance: PropTypes.number,
+  onDeposit: PropTypes.func,
+  onWithdraw: PropTypes.func
+}
+
+class BankAppContainer extends Component {
+  componentDidMount() {
+    this.unsubscribe = bankStore.subscribe(() =>
+      this.setState({balance: bankStore.getState().balance})
+    )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return(
+      <BankApp balance={bankStore.getState().balance}
+              onDeposit={ (amount) => bankStore.dispatch(bankActionCreators.depositIntoAccount(amount)) }
+              onWithdraw={ (amount) => bankStore.dispatch(bankActionCreators.withdrawFromAccount(amount)) }
+      />
+    )
+  }
+}
+
+render(<BankAppContainer />, document.getElementById('root'));
