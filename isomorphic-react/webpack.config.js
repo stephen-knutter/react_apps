d511e5b554d468050b2c219ec846a2c9ccc12d82
smkqp8 <stephen.knutter@gmail.com>
2016-11-23 07:41:22 -0700

    finish isomorphic render

---
+++ isomorphic-react/webpack.config.js
@@ -0,0 +1,17 @@
+let webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './browser.js'
+  ],
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel'
+    }]
+  }
+}
